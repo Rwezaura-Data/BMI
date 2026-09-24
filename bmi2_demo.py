fdd00fce7609ba2efe718eb65744489ab8742458
Rwezaura-Data <justine.jacks@gmail.com>
2026-09-24 11:48:56 +0300

    addded y var

--- bmi2_demo.py
+++ bmi2_demo.py
@@ -26,8 +26,8 @@ def heightConvert(height):
 
 # Calculate BMI
 if st.button("Calculate BMI :)"):
-    y = float(heightConvert(hgt))
-    bmi = wgt / (y ** 2)
+    #y = float(heightConvert(hgt))
+    bmi = wgt / (heightConvert(hgt) ** 2)
 
     st.write(f"### Your BMI is: **{bmi:.2f}**")
 
